8d2600a74ebfa54d4967e62c587442600d40fa19
himura467 <mitarashidango0927@gmail.com>
2025-12-30 18:41:39 +0900

    feat(bindings): RRule getters other than freq

--- src/bindings/rrule.rs
+++ src/bindings/rrule.rs
@@ -20,4 +20,72 @@ impl RRule {
     pub fn freq(&self) -> Frequency {
         self.rrule.freq.into()
     }
+
+    #[napi(getter)]
+    pub fn until(&self) -> Option<String> {
+        self.rrule.until.clone().map(|zdt| {
+            let rfc: core::rfc9557::Rfc9557 = zdt.into();
+            rfc.to_string()
+        })
+    }
+
+    #[napi(getter)]
+    pub fn count(&self) -> Option<u32> {
+        self.rrule.count
+    }
+
+    #[napi(getter)]
+    pub fn interval(&self) -> Option<u16> {
+        self.rrule.interval
+    }
+
+    #[napi(getter)]
+    pub fn by_second(&self) -> Vec<u8> {
+        self.rrule.by_second.clone()
+    }
+
+    #[napi(getter)]
+    pub fn by_minute(&self) -> Vec<u8> {
+        self.rrule.by_minute.clone()
+    }
+
+    #[napi(getter)]
+    pub fn by_hour(&self) -> Vec<u8> {
+        self.rrule.by_hour.clone()
+    }
+
+    #[napi(getter)]
+    pub fn by_day(&self) -> Vec<String> {
+        self.rrule.by_day.iter().map(|wn| wn.to_string()).collect()
+    }
+
+    #[napi(getter)]
+    pub fn by_month_day(&self) -> Vec<i8> {
+        self.rrule.by_month_day.clone()
+    }
+
+    #[napi(getter)]
+    pub fn by_year_day(&self) -> Vec<i16> {
+        self.rrule.by_year_day.clone()
+    }
+
+    #[napi(getter)]
+    pub fn by_week_no(&self) -> Vec<i8> {
+        self.rrule.by_week_no.clone()
+    }
+
+    #[napi(getter)]
+    pub fn by_month(&self) -> Vec<u8> {
+        self.rrule.by_month.clone()
+    }
+
+    #[napi(getter)]
+    pub fn by_set_pos(&self) -> Vec<i32> {
+        self.rrule.by_set_pos.clone()
+    }
+
+    #[napi(getter)]
+    pub fn wkst(&self) -> Option<String> {
+        self.rrule.wkst.map(|w| w.to_string())
+    }
 }
