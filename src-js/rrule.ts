069fd06ab4221474e52c4150d96b194121c3c5c6
himura467 <akitoshitara@gmail.com>
2026-01-31 17:08:47 +0900

    refactor(js): use fromNative helper instead of Object.create

--- src-js/rrule.ts
+++ src-js/rrule.ts
@@ -8,10 +8,14 @@ export class RRule {
     this.native = new NativeRRule(freq);
   }
 
-  static fromString(s: string): RRule {
-    const instance = Object.create(RRule.prototype);
-    instance.native = NativeRRule.fromString(s);
+  private static fromNative(native: NativeRRule): RRule {
+    const instance = new RRule(native.freq);
+    instance.native = native;
     return instance;
+  }
+
+  static fromString(s: string): RRule {
+    return RRule.fromNative(NativeRRule.fromString(s));
   }
 
   get freq(): Frequency {
